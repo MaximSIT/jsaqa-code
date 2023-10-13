fdcf09b08b495212c66e8fe64fed21604ea34035
Maxim <max_dancer94@mail.ru>
2023-10-13 21:44:52 +0500

    Commit for check

--- 7.3/playwright/NetologyTest.js
+++ 7.3/playwright/NetologyTest.js
@@ -1,16 +1,21 @@
 const { chromium } = require("playwright");
+const mail = require("./user.js");
+const password = require("./user.js");
 
 (async () => {
   const browser = await chromium.launch({
     headless: false,
     slowMo: 5000,
-    devtools: true
+    devtools: true,
   });
   const page = await browser.newPage();
   await page.goto("https://netology.ru");
-  await page.click("text=Каталог курсов");
-  await page.pause();
+  await page.click("text=Войти");
+  await page.click('[placeholder="Email"]');
+  await page.fill('[placeholder="Email"]', mail);
+  await page.click('[placeholder="Пароль"]');
+  await page.fill('[placeholder="Пароль"]', password);
 
-  //assertion
+  //await page.pause();
   await browser.close();
 })();
